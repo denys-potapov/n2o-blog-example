9b5f915da5b8a9c0ec9cfe1083a69be27166a1d6
Денис <denys.potapov@gmail.com>
2015-10-23 18:47:57 +0300

    adding posts comments

--- apps/sample/src/post.erl
+++ apps/sample/src/post.erl
@@ -8,4 +8,17 @@ post_id() -> wf:q(<<"id">>).
 
 main() -> 
 	Post = posts:get(wf:to_integer(post_id())),
-	#dtl{file="post", bindings=[{title, Post#post.title}, {text, Post#post.text}]}.
+	#dtl{file="post", bindings=[{title, Post#post.title}, {text, Post#post.text}, {comments, comments()}]}.
+
+comments() ->
+	[#textarea{id=comment, class=["form-control"], rows=3},
+      #button{id=send, class=["btn", "btn-default"], body="Post comment",postback=comment,source=[comment]} ].
+
+event(init) ->
+	wf:reg({post, post_id()});
+
+event(comment) -> 
+	wf:send({post, post_id()}, {client, wf:q(comment)});
+
+event({client, Text}) ->
+	wf:insert_bottom(comments, #blockquote{body = #p{body = wf:html_encode(wf:jse(Text))}}).
